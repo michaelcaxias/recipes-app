142cb29b07ffcdb3a6a28e956a52d2ca651ea9c1
Michael <michaelcaxias.dev@gmail.com>
2021-11-05 17:51:03 -0300

    Requisito 13: Completo

---
+++ src/components/HeaderSearchBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default function HeaderSearchBar() {
+  return (
+    <header>
+      <label htmlFor="ingredient-radio">
+        Busca por Ingredientes:
+        <input
+          type="radio"
+          data-testid="ingredient-search-radio"
+          id="ingredient-radio"
+        />
+      </label>
+      <label htmlFor="name-radio">
+        Busca por Nome:
+        <input
+          type="radio"
+          data-testid="name-search-radio"
+          id="name-radio"
+        />
+      </label>
+      <label htmlFor="first-letter-radio">
+        Busca por primeira letra:
+        <input
+          type="radio"
+          data-testid="first-letter-search-radio"
+          id="first-letter-radio"
+        />
+      </label>
+      <label htmlFor="search-btn">
+        <input
+          type="button"
+          data-testid="exec-search-btn"
+          id="search-btn"
+        />
+      </label>
+    </header>
+  );
+}
