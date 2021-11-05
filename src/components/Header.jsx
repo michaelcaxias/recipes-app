7703561497526acdb0b227e31f3bc0c6b46e32cb
Michael <michaelcaxias.dev@gmail.com>
2021-11-05 18:43:14 -0300

    Requisito 11: Completo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,39 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useHistory } from 'react-router';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-export default function Header({ title }) {
+export default function Header({ title, searchButton = true }) {
+  const history = useHistory();
+  const searchButtonElement = (
+    <button
+      type="button"
+      data-testid="search-top-btn"
+      onClick=""
+    >
+      <img src={ searchIcon } alt="search" />
+    </button>
+  );
   return (
     <header>
-      <h1 data-testid="page-title">{ title }</h1>
       <button
         type="button"
         data-testid="profile-top-btn"
-        onClick=""
+        onClick={ () => history.push('/perfil') }
       >
         <img src={ profileIcon } alt="profile" />
       </button>
-      <button
-        type="button"
-        data-testid="search-top-btn"
-        onClick=""
-      >
-        <img src={ searchIcon } alt="search" />
-      </button>
+      <h1 data-testid="page-title">{ title }</h1>
+      { searchButton && searchButtonElement }
     </header>
   );
 }
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+  searchButton: PropTypes.bool.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
