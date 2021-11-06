1ff660c773419a46ccb924c974e37dd7de74bca1
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-11-05 22:10:01 -0300

    Inicio da estilizacao da pagina de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
+import '../styles/login.css';
 
 export default function Login() {
   const history = useHistory();
@@ -19,28 +20,29 @@ export default function Login() {
   const MAX_LENGTH = 6;
   const disabled = password.length > MAX_LENGTH && isEmailValid(email);
   return (
-    <form>
-      <input
-        type="email"
-        data-testid="email-input"
-        value={ email }
-        onChange={ ({ target: { value } }) => setEmail(value) }
-      />
-      <input
-        type="password"
-        data-testid="password-input"
-        value={ password }
-        onChange={ ({ target: { value } }) => setPassword(value) }
-      />
-      <button
-        type="button"
-        data-testid="login-submit-btn"
-        disabled={ !disabled }
-        onClick={ setLocalStorage }
-      >
-        Entrar
-
-      </button>
-    </form>
+    <section className="login-section">
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ ({ target: { value } }) => setEmail(value) }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ ({ target: { value } }) => setPassword(value) }
+        />
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          disabled={ !disabled }
+          onClick={ setLocalStorage }
+        >
+          Entrar
+        </button>
+      </form>
+    </section>
   );
 }
