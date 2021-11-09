17512feba60f54caa9a321b71e760c127ab01aab
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-11-09 19:02:31 -0300

    Fixes foodrecipes

--- src/pages/FoodRecipes.jsx
+++ src/pages/FoodRecipes.jsx
@@ -1,21 +1,23 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import CategoryButtons from '../components/CategoryButtons';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 import RecipeCard from '../components/RecipeCard';
 import requestRecipes from '../helpers/requestRecipes';
-import recipesContext from '../context/recipesContext';
-import '../styles/foodAndDrinks.css';
+// import recipesContext from '../context/recipesContext';
 
 const MAX_RECIPES = 12;
-const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=miscellaneous';
 const key = 'meals';
 
 export default function FoodRecipes() {
-  const { data, setData } = useContext(recipesContext);
+  const [meals, setMeals] = useState([]);
   useEffect(() => {
-    requestRecipes(url, data, setData, key);
+    requestRecipes(url, meals, setMeals, key);
   }, []);
+
+  // const { data } = useContext(recipesContext);
+  const recipes = meals ? meals.slice(0, MAX_RECIPES) : [];
   return (
     <section>
       <Header title="Comidas" />
