b5af2aa9defee0f13ce957d643863bb39f9dfe98
Michael <michaelcaxias.dev@gmail.com>
2021-11-11 16:10:39 -0300

    Implementa lógica para IngredientsMeasureList

--- src/components/IngredientsMeasureList.jsx
+++ src/components/IngredientsMeasureList.jsx
@@ -2,28 +2,24 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default function IngredientsMeasureList({ ingredients }) {
+  // const [isChecked, setIsChecked] = useState(false);
+
   const arrayKeys = Object.keys(ingredients);
   const ingredientsKeys = arrayKeys.filter((key) => key.includes('strIngredient'));
   const ingredientMeasureKeys = arrayKeys.filter((key) => key.includes('strMeasure'));
 
+  // const isChecked = handleCheckbox(event);
   return (
     <ul>
       { ingredientsKeys.map((key, index) => (
         ingredients[key] && (
-          <label
+          <li
             htmlFor={ key }
             key={ key }
-            data-testid={ `${index}-ingredient-name-and-measure`}
+            data-testid={ `${index}-ingredient-name-and-measure` }
           >
             { `${ingredients[key]} - ${ingredients[ingredientMeasureKeys[index]]}` }
-            <input
-              type="checkbox"
-              id={ key }
-              name={ key }
-              checked={ false }
-              // onChange={}
-            />
-          </label>
+          </li>
         )))}
     </ul>
   );
