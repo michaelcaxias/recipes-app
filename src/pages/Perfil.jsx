abb64ee5d656dccf256499715ae86a3736d0fb94
Michael <michaelcaxias.dev@gmail.com>
2021-11-11 19:13:49 -0300

    Requisito 82 e 84 Completos

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -6,6 +6,25 @@ export default function Perfil() {
   return (
     <section>
       <Header title="Perfil" searchButton={ false } />
+      <h1 data-testid="profile-email">email@gmail.com</h1>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Receitas Feitas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Sair
+      </button>
       <FooterMenu />
     </section>
   );
