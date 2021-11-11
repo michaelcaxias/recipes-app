8c3648241f5b366b106e3d6b0310b08646caf1b7
Michael <michaelcaxias.dev@gmail.com>
2021-11-11 19:19:56 -0300

    Requisito 83: Completo

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 
 export default function Perfil() {
+  const [userEmail, setUserEmail] = useState('');
+
+  useEffect(() => {
+    const emailStorage = localStorage.getItem('user');
+    if (emailStorage) {
+      const { email } = JSON.parse(emailStorage);
+      setUserEmail(email);
+    }
+  }, []);
+
   return (
     <section>
       <Header title="Perfil" searchButton={ false } />
-      <h1 data-testid="profile-email">email@gmail.com</h1>
+      <h1 data-testid="profile-email">{ userEmail }</h1>
       <button
         type="button"
         data-testid="profile-done-btn"
