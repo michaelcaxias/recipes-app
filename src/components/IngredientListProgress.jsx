b9b1d07bf5b64241689f7786897465570a68854e
Michael <michaelcaxias.dev@gmail.com>
2021-11-11 16:22:40 -0300

    Adiciona componente IngredientListProgress

---
+++ src/components/IngredientListProgress.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function IngredientListProgress(ingredients) {
+  // const [isChecked, setIsChecked] = useState(false);
+
+  const arrayKeys = Object.keys(ingredients);
+  const ingredientsKeys = arrayKeys.filter((key) => key.includes('strIngredient'));
+  const ingredientMeasureKeys = arrayKeys.filter((key) => key.includes('strMeasure'));
+
+  function handleCheckbox({ target: { checked = false } }) {
+    const isChecked = checked;
+    console.log(isChecked);
+    return !isChecked;
+  }
+
+  // const isChecked = handleCheckbox(event);
+  return (
+    <ul>
+      { ingredientsKeys.map((key, index) => (
+        ingredients[key] && (
+          <label
+            htmlFor={ key }
+            key={ key }
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            <input
+              type="checkbox"
+              id={ key }
+              name={ key }
+              checked={ handleCheckbox }
+              onChange={ ({ target: checked }) => {
+                handleCheckbox({ target: { checked } });
+              } }
+            />
+            { `${ingredients[key]} - ${ingredients[ingredientMeasureKeys[index]]}` }
+          </label>
+        )))}
+    </ul>
+  );
+}
+
+IngredientListProgress.propTypes = {
+  ingredients: PropTypes.objectOf(
+    PropTypes.shape(),
+  ).isRequired,
+};
