d6128b0925885ed8cc230ac0250ba1519ec7bfe1
Michael <michaelcaxias.dev@gmail.com>
2021-11-12 12:25:44 -0300

    Requisitos 70 e 71: Completos

--- src/pages/ExplorerDrinks.jsx
+++ src/pages/ExplorerDrinks.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 
 export default function ExplorerDrinks() {
+  const history = useHistory();
   return (
     <section>
       <Header title="Explorar Bebidas" searchButton={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/receitas-feitas') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push('/receitas-favoritas') }
+      >
+        Me Surpreenda!
+      </button>
       <FooterMenu />
     </section>
   );
--- src/pages/ExplorerFoods.jsx
+++ src/pages/ExplorerFoods.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import FooterMenu from '../components/FooterMenu';
 import Header from '../components/Header';
 
 export default function ExplorerFoods() {
+  const history = useHistory();
   return (
     <section>
       <Header title="Explorar Comidas" searchButton={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/receitas-feitas') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-area"
+        onClick={ () => history.push('/receitas-favoritas') }
+      >
+        Por Local de Origem
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push('/receitas-favoritas') }
+      >
+        Me Surpreenda!
+      </button>
       <FooterMenu />
     </section>
   );
