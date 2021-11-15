b699bc50dae50e22853e7a4c0667b9c92b10eb71
Cecilia Cacais <ceci.cacais@gmail.com>
2021-11-15 13:00:19 -0300

    Implementa pagina de favoritos

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -2,5 +2,21 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default function FavoriteRecipes() {
-  return <Header title="Receitas Favoritas" searchButton={ false } />;
+  return (
+    <div>
+      <Header title="Receitas Favoritas" searchButton={ false } />
+      <button type="button" data-testid="filter-by-all-btn">All</button>
+      <button type="button" data-testid="filter-by-food-btn">Food</button>
+      <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      <button type="button" data-testid={ `${index}-horizontal-share-btn` }>
+        Compartilhar
+      </button>
+      <img data-testid={ `${index}-horizontal-image` } src={ image } />
+      <p data-testid={ `${index}-horizontal-top-text` }>Categoria</p>
+      <h1 data-testid={ `${index}-horizontal-name` }>Nome</h1>
+      <p data-testid={ `${index}-horizontal-done-date` }>Data</p>
+      <p data-testid={ `${index}-${tagName}-horizontal-tag` }>Tags</p>
+    </div>
+
+  );
 }
