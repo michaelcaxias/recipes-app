c4c912f36837265d378e6ded11e1f1f4d0a0ebf8
Michael <michaelcaxias.dev@gmail.com>
2021-11-16 15:58:05 -0300

    Requisitos 64 ao 65

--- src/components/CardFavorites.jsx
+++ src/components/CardFavorites.jsx
@@ -1,25 +1,40 @@
 import PropTypes, { any, shape } from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
 
-export default function CardFavorites({ favorite, index }) {
+export default function CardFavorites({ favorite, index, setFavorites }) {
+  function disfavor() {
+    const favoritesStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    favoritesStorage.splice(index, 1);
+    console.log(favoritesStorage);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoritesStorage));
+    setFavorites(favoritesStorage);
+  }
+
   return (
     <div>
       {favorite.type === 'comida'
         ? (
           <div>
-            <img
-              data-testid={ `${index}-horizontal-image` }
-              alt={ `Imagem da comida ${favorite.name}` }
-              src={ favorite.image }
-            />
-            <h1 data-testid={ `${index}-horizontal-name` }>{ favorite.name }</h1>
-            <p
-              data-testid={ `${index}-horizontal-top-text` }
+            <Link
+              to={ `/comidas/${favorite.id}` }
             >
-              { `${favorite.area} - ${favorite.category}` }
-            </p>
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                alt={ `Imagem da comida ${favorite.name}` }
+                src={ favorite.image }
+              />
+              <h1 data-testid={ `${index}-horizontal-name` }>{ favorite.name }</h1>
+
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                { `${favorite.area} - ${favorite.category}` }
+              </p>
+            </Link>
+
             <button type="button">
               <img
                 data-testid={ `${index}-horizontal-share-btn` }
@@ -27,7 +42,10 @@ export default function CardFavorites({ favorite, index }) {
                 src={ shareIcon }
               />
             </button>
-            <button type="button">
+            <button
+              type="button"
+              onClick={ disfavor }
+            >
               <img
                 data-testid={ `${index}-horizontal-favorite-btn` }
                 alt="desfavoritar"
@@ -37,17 +55,21 @@ export default function CardFavorites({ favorite, index }) {
           </div>)
         : (
           <div>
-            <img
-              data-testid={ `${index}-horizontal-image` }
-              alt={ `Imagem da bebida ${favorite.name}` }
-              src={ favorite.image }
-            />
-            <h1 data-testid={ `${index}-horizontal-name` }>{ favorite.name }</h1>
-            <p
-              data-testid={ `${index}-horizontal-top-text` }
+            <Link
+              to={ `/bebidas/${favorite.id}` }
             >
-              { favorite.alcoholicOrNot }
-            </p>
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                alt={ `Imagem da bebida ${favorite.name}` }
+                src={ favorite.image }
+              />
+              <h1 data-testid={ `${index}-horizontal-name` }>{ favorite.name }</h1>
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                { favorite.alcoholicOrNot }
+              </p>
+            </Link>
             <button type="button">
               <img
                 data-testid={ `${index}-horizontal-share-btn` }
@@ -55,7 +77,10 @@ export default function CardFavorites({ favorite, index }) {
                 src={ shareIcon }
               />
             </button>
-            <button type="button">
+            <button
+              type="button"
+              onClick={ disfavor }
+            >
               <img
                 data-testid={ `${index}-horizontal-favorite-btn` }
                 alt="desfavoritar"
@@ -70,4 +95,5 @@ export default function CardFavorites({ favorite, index }) {
 CardFavorites.propTypes = {
   favorite: PropTypes.arrayOf(shape(any)).isRequired,
   index: PropTypes.number.isRequired,
+  setFavorites: PropTypes.func.isRequired,
 };
