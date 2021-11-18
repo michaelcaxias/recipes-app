a61e93e4ab05d4dffc2fd50234c151cbff31d497
Michael <michaelcaxias.dev@gmail.com>
2021-11-18 17:50:27 -0300

    Refatorando Login e CSS

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,12 @@ import { AiOutlineUser, AiOutlineUnlock } from 'react-icons/ai';
 import wave from '../images/wave';
 import '../styles/login.css';
 
+const localStorageSetItem = (key, initialValue) => {
+  if (!localStorage.getItem(!key)) {
+    localStorage.setItem(key, JSON.stringify(initialValue));
+  }
+};
+
 export default function Login() {
   const history = useHistory();
   const [email, setEmail] = useState('');
@@ -17,18 +23,9 @@ export default function Login() {
     const objectEmail = { email };
     localStorage.setItem('user', JSON.stringify(objectEmail));
 
-    if (!localStorage.getItem('doneRecipes')) {
-      localStorage.setItem('doneRecipes', JSON.stringify([]));
-    }
-
-    if (!localStorage.getItem('favoriteRecipes')) {
-      localStorage.setItem('favoriteRecipes', JSON.stringify([]));
-    }
-
-    if (!localStorage.getItem('inProgressRecipes')) {
-      localStorage
-        .setItem('inProgressRecipes', JSON.stringify({ cocktails: {}, meals: {} }));
-    }
+    localStorageSetItem('doneRecipes', []);
+    localStorageSetItem('favoriteRecipes', []);
+    localStorageSetItem('inProgressRecipes', { cocktails: {}, meals: {} });
 
     history.push('/comidas');
   };
@@ -36,42 +33,47 @@ export default function Login() {
   const MAX_LENGTH = 6;
   const disabled = password.length > MAX_LENGTH && isEmailValid(email);
   return (
-    <section className="login-section">
-      <form>
-        <section className="input-section">
-          <AiOutlineUser className="react-login-icon" />
-          <input
-            type="email"
-            data-testid="email-input"
-            value={ email }
-            placeholder="Email"
-            onChange={ ({ target: { value } }) => setEmail(value) }
-          />
-        </section>
-        <section className="input-section">
-          <AiOutlineUnlock className="react-login-icon" />
-          <input
-            type="password"
-            data-testid="password-input"
-            value={ password }
-            placeholder="Senha"
-            onChange={ ({ target: { value } }) => setPassword(value) }
-          />
-        </section>
-        <button
-          type="button"
-          data-testid="login-submit-btn"
-          disabled={ !disabled }
-          onClick={ setLocalStorage }
-        >
-          Entrar
-        </button>
-      </form>
-      <div className="custom-shape-divider-bottom-1636461259">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d={ wave } className="shape-fill" />
-        </svg>
-      </div>
-    </section>
+    <>
+      <section className="login-section">
+        <form className="forms">
+          <section className="input-section">
+            <AiOutlineUser className="react-login-icon" />
+            <input
+              type="email"
+              data-testid="email-input"
+              value={ email }
+              placeholder="Email"
+              onChange={ ({ target: { value } }) => setEmail(value) }
+            />
+          </section>
+          <section className="input-section">
+            <AiOutlineUnlock className="react-login-icon" />
+            <input
+              type="password"
+              data-testid="password-input"
+              value={ password }
+              placeholder="Senha"
+              onChange={ ({ target: { value } }) => setPassword(value) }
+            />
+          </section>
+          <button
+            type="button"
+            data-testid="login-submit-btn"
+            disabled={ !disabled }
+            onClick={ setLocalStorage }
+          >
+            Entrar
+          </button>
+        </form>
+        <div className="custom-shape-divider-bottom-1636461259">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d={ wave } className="shape-fill" />
+          </svg>
+        </div>
+      </section>
+      <section className="login-aside-desktop">
+        <p>a</p>
+      </section>
+    </>
   );
 }
